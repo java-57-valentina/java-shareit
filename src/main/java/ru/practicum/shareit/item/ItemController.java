8b6e04a0d2862c821e79d87a114d4b408a0dc454
Valentina Svistunova <svistunova.vv@yandex.com>
2025-06-29 13:29:48 +0300

    Implementd booking * comments

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,11 +1,15 @@
 package ru.practicum.shareit.item;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentRequestDto;
+import ru.practicum.shareit.item.dto.CommentResponseDto;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemResponseDto;
 import ru.practicum.shareit.user.validation.Create;
 import ru.practicum.shareit.user.validation.Update;
 
@@ -22,9 +26,9 @@ public class ItemController {
     public static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
 
     @PostMapping
-    public ItemDto add(@RequestHeader(X_SHARER_USER_ID) long ownerId,
+    public ItemDto add(@RequestHeader(X_SHARER_USER_ID) long userId,
                        @Validated({Create.class}) @RequestBody ItemDto itemDto) {
-        ItemDto added = itemService.add(itemDto, ownerId);
+        ItemDto added = itemService.add(itemDto, userId);
         log.info("Item was added: {}", added);
         return added;
     }
@@ -40,23 +44,37 @@ public class ItemController {
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto getById(@PathVariable @Min(1) Long itemId) {
-        return itemService.getById(itemId);
+    public ItemResponseDto getById(
+            @PathVariable @Min(1) Long itemId,
+            @RequestHeader(value = X_SHARER_USER_ID, required = false) Long userId) {
+        return itemService.getById(itemId, userId);
     }
 
     @PatchMapping("/{itemId}")
     public ItemDto update(
             @PathVariable @Min(1) Long itemId,
-            @RequestHeader(X_SHARER_USER_ID) long ownerId,
+            @RequestHeader(X_SHARER_USER_ID) long userId,
             @Validated({Update.class}) @RequestBody ItemDto itemDto) {
-        ItemDto updated = itemService.update(itemId, ownerId, itemDto);
+        ItemDto updated = itemService.update(itemId, userId, itemDto);
         log.info("Item was updated: {}", updated);
         return updated;
     }
 
     @GetMapping("/search")
-    public Collection<ItemDto> searchItems(@RequestHeader(X_SHARER_USER_ID) Long ownerId,
-                                     @RequestParam(name = "text") String text) {
-        return itemService.search(ownerId, text);
+    public Collection<ItemDto> searchItems(
+            @RequestHeader(X_SHARER_USER_ID) Long userId,
+            @RequestParam(name = "text") String text) {
+        return itemService.search(userId, text);
     }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentResponseDto addComment(
+            @PathVariable @Min(1) Long itemId,
+            @RequestHeader(X_SHARER_USER_ID) long userId,
+            @Valid @RequestBody CommentRequestDto comment) {
+        CommentResponseDto commentResponseDto = itemService.addComment(userId, itemId, comment);
+        log.info("Comment was added: {}", commentResponseDto);
+        return commentResponseDto;
+    }
+
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,15 +1,31 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "items")
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "description", nullable = false)
     private String description;
+
     private Boolean available;
+
+    @Column(name = "user_id", nullable = false)
     private Long ownerId;
 }
