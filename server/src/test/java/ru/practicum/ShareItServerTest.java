4f64b027217810bd7cde7e8143a1310cb417ee8d
Valentina Svistunova <svistunova.vv@yandex.com>
2025-07-12 11:46:06 +0300

    Fix code style

--- server/src/test/java/ru/practicum/ShareItServerTest.java
+++ server/src/test/java/ru/practicum/ShareItServerTest.java
@@ -4,33 +4,17 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple ShareItServer.
- */
-public class ShareItServerTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+public class ShareItServerTest extends TestCase {
     public ShareItServerTest(String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( ShareItServerTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
     {
         assertTrue( true );
